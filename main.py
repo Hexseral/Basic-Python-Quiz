23a9bab0bf254dfda45e6a3ef67032d1ad52a0c1
hexseral <jrtatum0133@eagle.fgcu.edu>
2019-09-17 12:38:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Jordan Tatum
+# This program will be a self grading quiz based on basic Algebra 1 arithmatic functions, due to it still being in a trial stage the first
+# Equation is simple in order for me to figure out how to structure the quiz and deal with the bugs
+name = input("Hello User welcome to the Algebra 1 Knowlege Quiz, please start by entering your name\n")
+print("Hello,", name, "Answer these questions as fast as you can! Good luck!\n")
+answerOne = input("what is 1 + 1?\n")
+numOne = int(answerOne)
+print(numOne == 2)
