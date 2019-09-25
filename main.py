5414aa93937a3f519cd55ba67ece8df0fa0fa037
Hexseral <54594278+Hexseral@users.noreply.github.com>
2019-09-25 19:20:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # Jordan Tatum
-# This program will be a self grading quiz based on basic Algebra 1 arithmatic functions, due to it still being in a trial stage the first
+# This program will be a self grading quiz based on basic Algebra 1 arethmatic functions, due to it still being in a trial stage the first
 # Equation is simple in order for me to figure out how to structure the quiz and deal with the bugs
-name = input("Hello User welcome to the Algebra 1 Knowlege Quiz, please start by entering your name\n")
-print("Hello,", name, "Answer these questions as fast as you can! Good luck!\n")
+
+name = input("Hello User welcome to the Algebra 1 Knowledge Quiz, please start by entering your name\n")
+
+#Starts the test, the list is a joke if the user tries to be funny and input something besides the varied versions of start in the list
+print("Hello,", name, "please type start to begin the test, Answer the questions as best as you can.")
+List = ("Start", "start", "START", "sTART", "StArT", "sTaRt", "sTART", "StART", "STaRT", "STArT", "STARt")
+start = input()
+if start in List:
+    print("Good Luck!")
+else:
+    print("Too bad, start anyway!")
+
 answerOne = input("what is 1 + 1?\n")
-numOne = int(answerOne)
-print(numOne == 2)
+if answerOne == 2:
+    print("Correct!")
+else:
+    print("Incorrect.")
