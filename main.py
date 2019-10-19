830ef65e87f4c9e1715bf704b6e242ead36fdebe
Hexseral <54594278+Hexseral@users.noreply.github.com>
2019-10-19 19:43:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # creates a list (Container) of question objects (questions, options, answers)
     questions = []
 
-    q1 = Question(a = 1, o =[0, 1, 5, 6], q = "What is 6 divided by 6?")
+    q1 = Question(a = 15, o =[7, 18, 20, 15], q = "What is 90 divided by 6?")
     # adds question one into the list.
     questions.append(q1)
 
@@ -40,19 +40,19 @@ if __name__ == '__main__':
     # adds question two into the list.
     questions.append(q2)
 
-    q3 = Question(a = 6, o =[4, 3, 6, 8], q = "What is the square root of 36?")
+    q3 = Question(a = 870, o =[560, 870, 300, 2], q = "What is 145 times 6?")
     # adds question three into the list.
     questions.append(q3)
 
-    q4 = Question(a = 6, o =[4, 3, 6, 8], q = "What is the square root of 36?")
+    q4 = Question(a = 5320, o =[4523, 3452, 8423, 5320], q = "What is 56 times 95?")
     # adds question four into the list.
     questions.append(q4)
 
-    q5 = Question(a = 6, o =[4, 3, 6, 8], q = "What is the square root of 36?")
+    q5 = Question(a = 16384, o =[16384, 12560, 90000, 14435], q = "What is 128 squared?")
     # adds question five into the list.
     questions.append(q5)
 
-    name = input("Hello! \n Welcome to the SAT General Knowledge Practice Test.\n Please start by entering your Name:\n")
+    name = input("Hello! \n Welcome to the General Knowledge Practice Test.\n Please start by entering your Name:\n")
 
     #Starts the test, the list is a joke if the user tries to be funny and input something besides the varied versions of start in the list
     print("Hello", name + "!", "\nAnswer the following questions as best as you can. \n Please type start to begin the test.")
@@ -73,4 +73,6 @@ if __name__ == '__main__':
             print("Correct!\n")
             total += 1
         else:
-            print("Incorrect.", "the Correct answer was:", x.answer)
+            print("Incorrect.", "the Correct answer was:\n", x.answer, "\n")
+
+    print("You scored a", total, "out of 5!",)
