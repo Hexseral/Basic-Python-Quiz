e5e255e28f5e834f86d922a13a3a34194e7a62d9
Hexseral <54594278+Hexseral@users.noreply.github.com>
2019-11-24 19:30:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Jordan Tatum
-# This program will be a self grading quiz. I learned how to use classes, constructors, lists, and methods in order to condense this program
-# I still plan to add a lot more, the questions are basic for now so I can work on formatting the test as well as creating responses from user input errors.
-# I spent a lot of time researching, reading, and discussing concepts with my peers. ALL OF THIS CODE WAS WRITTEN BY ME. My peers only helped me conceptualize.
+# This program will be a self grading quiz. I learned how to use classes, constructors, lists, and methods
+# in order to condense this program
+# I added a user input error catcher that allows the program to not be broken by strings or bad input
+# I spent a lot of time researching, reading, and discussing concepts with my peers. ALL OF THIS CODE WAS WRITTEN BY ME.
+# My peers only helped me conceptualize.
 # Here are some of the sources I used:
 # https://docs.python.org/3/tutorial/classes.html
 # https://docs.python.org/3/tutorial/datastructures.html
@@ -21,8 +23,13 @@ class Question:
     def question_print(self):
         print(self.question)
         print(self.option)
-
-
+    def isInteger(i):
+        try:
+            int(i)
+            return True
+        except ValueError:
+            return False
+    # Checking if variable is a string in order to make the program not crash
 if __name__ == '__main__':
 
     # Used later on for quiz grade & percentage.
@@ -32,47 +39,83 @@ if __name__ == '__main__':
     # creates a list (Container) of question objects (questions, options, answers)
     questions = []
 
-    q1 = Question(a = 15, o =[7, 18, 20, 15], q = "What is 90 divided by 6?")
+    q1 = Question(a = 15, o =[7, 18, 20, 15], q = "What is 90 divided by 6?\n")
     # adds question one into the list.
     questions.append(q1)
 
-    q2 = Question(a = 6, o =[4, 3, 6, 8], q = "What is the square root of 36?")
+    q2 = Question(a = 6, o =[4, 3, 6, 8], q = "What is the square root of 36?\n")
     # adds question two into the list.
     questions.append(q2)
 
-    q3 = Question(a = 870, o =[560, 870, 300, 2], q = "What is 145 times 6?")
+    q3 = Question(a = 870, o =[560, 870, 300, 2], q = "What is 145 times 6?\n")
     # adds question three into the list.
     questions.append(q3)
 
-    q4 = Question(a = 5320, o =[4523, 3452, 8423, 5320], q = "What is 56 times 95?")
+    q4 = Question(a = 5320, o =[4523, 3452, 8423, 5320], q = "What is 56 times 95?\n")
     # adds question four into the list.
     questions.append(q4)
 
-    q5 = Question(a = 16384, o =[16384, 12560, 90000, 14435], q = "What is 128 squared?")
+    q5 = Question(a = 16384, o =[16384, 12560, 90000, 14435], q = "What is 128 squared?\n")
     # adds question five into the list.
     questions.append(q5)
 
-    name = input("Hello! \n Welcome to the General Knowledge Practice Test.\n Please start by entering your Name:\n")
+    q6 = Question(a = 160, o =[160, 340, 124, 185], q = "If y = 16x + 32, and x = 8 what is y?\n")
+    # adds question six into the list.
+    questions.append(q6)
 
-    #Starts the test, the list is a joke if the user tries to be funny and input something besides the varied versions of start in the list
-    print("Hello", name + "!", "\nAnswer the following questions as best as you can. \n Please type start to begin the test.")
+    q7 = Question(a = 156, o =[143, 112, 205, 156], q = "x + 156 = 312, what is x?\n")
+    # adds question seven into the list.
+    questions.append(q7)
+
+    q8= Question(a = 11, o =[5, 16, 23, 11], q = "13x - 72 = 71, what is x? \n")
+    # adds question eight into the list.
+    questions.append(q8)
+
+    q9 = Question(a = 56, o =[16, 52, 46, 23], q = " 3x = 156, what is x?\n")
+    # adds question nine into the list.
+    questions.append(q9)
+
+    q10 = Question(a = 36, o =[14, 42, 36, 7], q = "y = 2(x + 6), x = 12, what is y?\n")
+    # adds question ten into the list.
+    questions.append(q10)
+
+    name = input("Hello! \n Welcome to the Basic Algebra Practice Test.\n Please start by entering your Name:\n")
+
+    # Starts the test, the list is a joke if the user tries to be funny and input something besides the varied versions
+    # of start in the list
+    print("\nHello", name + "!", "\nAnswer the following questions as best as you can. \n Please type start to begin the test.")
     #list of different syntax of start
     List = ("Start", "start", "START", "sTART", "StArT", "sTaRt", "sTART", "StART", "STaRT", "STArT", "STARt")
     start = input()
     if start in List:
         print("Good Luck!\n")
-    #fun little easter egg if someone thinks they will break the code
+    # fun little easter egg if someone thinks they will break the code
     else:
-        print("Too bad, start anyway!")
-    #Declares a total in order to add up the amount of correct questions for a final score.
+        print("Too bad, start anyway!\n")
+    # Declares a total in order to add up the amount of correct questions for a final score.
 
-    for x in questions:
+    # declared variable for incrementation
+    i = 0
+
+    while i < questions.__len__():
+        x = questions[i]
         x.question_print()
-        user_input = int(input())
-        if user_input == x.answer:
-            print("Correct!\n")
-            total += 1
+        user_input = input()
+        if Question.isInteger(user_input):
+            un = int(user_input)
+            if un == x.answer:
+                print("Correct!\n")
+                total += 1
+                i += 1
+                # increments the question for a total average and allows the loop to pass to prevent input error
+            else:
+                if un not in x.option:
+                    print("The answer you input is not an option, please try again."
+                          # Allows for input error to not be a possibility to break the program
+                else:
+                    print("Incorrect.")
         else:
-            print("Incorrect.", "the Correct answer was:\n", x.answer, "\n")
+            print("The answer you input is not a number\n")
 
-    print("You scored a", total, "out of 5!",)
+
+    print("You scored a", total, "out of 10!",)
